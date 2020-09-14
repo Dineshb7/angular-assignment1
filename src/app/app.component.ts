ff9aeab154b46d6aa0bead0b5009b72faa612fbc
Dineshb7 <bussadinesh@gmail.com>
2020-09-14 21:41:48 +0530

    Added No data card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,36 +9,43 @@ import { AppServiceService } from './app-service.service';
 export class AppComponent implements OnInit {
   public objects: any = []
   public buttons = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020];
+  public loading: boolean = true;
 
-  constructor(private appService: AppServiceService, ) { }
+  constructor(private appService: AppServiceService,) { }
   ngOnInit() {
     this.appService.defaultCall().subscribe((data) => {
-        this.objects = data
-      }),
+      this.objects = data
+      this.loading = false;
+    }),
       (err) => {
+        this.loading = false;
         console.log("Defaullt API Call is failed", err)
       }
   }
 
   selBtn(selectedYear) {
     this.appService.yearLaunchCall(selectedYear).subscribe((yearSeldata) => {
-        this.objects = yearSeldata;
-      }),
+      this.objects = yearSeldata;
+      this.loading = false;
+    }),
       (err) => {
+        this.loading = false;
         console.log("Year selcted Call is failed", err)
       }
   }
 
-  trackByFilterId(index,object){
+  trackByFilterId(index, object) {
     return object.flight_number;
   }
 
   launchSuccessBtn(launchSel) {
     if (launchSel == 'true') {
       this.appService.launchCall().subscribe((launchSuccessdata) => {
-          this.objects = launchSuccessdata;
-        }),
+        this.objects = launchSuccessdata;
+        this.loading = false;
+      }),
         (err) => {
+          this.loading = false;
           console.log("Launch selcted Call is failed", err)
         }
     }
@@ -47,10 +54,11 @@ export class AppComponent implements OnInit {
   landSuccessBtn(landSel) {
     if (landSel == 'true') {
       this.appService.landCall().subscribe((landSuccessdata) => {
-          this.objects = landSuccessdata
-          console.log("data LandSuccessdata Received", this.objects, landSuccessdata);
-        }),
+        this.objects = landSuccessdata
+        this.loading = false;
+      }),
         (err) => {
+          this.loading = false;
           console.log("Land selcted Call is failed", err)
         }
     }
